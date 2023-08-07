503f3e18967c5afca0337315e87f27dbac824d94
Yetunde Alese <yaalese1@gmail.com>
2023-08-07 16:32:10 -0400

    commit tree c5d023d292f4aba97a3ef77aad17941ac8bf81b9

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+// let companyName = "Scuber"
+const companyName = "Scuber"
+// const mostProfitableNeighborhood = "Chelsea"
+let mostProfitableNeighborhood = "Chelsea"
+
+let companyCeo = "Susan Smith"
